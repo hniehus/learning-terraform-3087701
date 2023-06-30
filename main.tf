06d5f7f7a3d6e25ad5abacd36108f3ccbb2559d8
hniehus <harald@niehus.org>
2023-06-30 20:04:36 +0200

    -

--- main.tf
+++ main.tf
@@ -14,44 +14,44 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+#module "vpc" {
+#  source = "terraform-aws-modules/vpc/aws"
+#
+#  name = "dev"
+#  cidr = "10.0.0.0/16"
+#
+#  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+#  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#
+#  enable_nat_gateway = true
+#
+#  tags = {
+#    Terraform = "true"
+#    Environment = "dev"
+#  }
+#}
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids =[module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  name    = "blog"
-
-  vpc_id = module.vpc.public_subnets[0]
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
+#resource "aws_instance" "blog" {
+#  ami           = data.aws_ami.app_ami.id
+#  instance_type = var.instance_type
+#  vpc_security_group_ids =[module.blog_sg.security_group_id]
+#
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
+#
+#module "blog_sg" {
+#  source  = "terraform-aws-modules/security-group/aws"
+#  version = "5.1.0"
+#  name    = "blog"
+#
+#  vpc_id = module.vpc.public_subnets[0]
+#
+#  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+#  ingress_cidr_blocks = ["0.0.0.0/0"]
+#
+#  egress_rules       = ["all-all"]
+#  egress_cidr_blocks = ["0.0.0.0/0"]
+#}
